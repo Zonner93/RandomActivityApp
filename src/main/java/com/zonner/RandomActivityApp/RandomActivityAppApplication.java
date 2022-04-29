fc00d2454fa4edeb7675a6ffaf602cb228b3d0c6
Adam Pawluczuk <adam.pawluczuk@ppuc.pl>
2022-04-29 16:24:00 +0200

    initial commit

---
+++ src/main/java/com/zonner/RandomActivityApp/RandomActivityAppApplication.java
@@ -0,0 +1,13 @@
+package com.zonner.RandomActivityApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomActivityAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RandomActivityAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zonner/RandomActivityApp/RandomActivityAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zonner.RandomActivityApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomActivityAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

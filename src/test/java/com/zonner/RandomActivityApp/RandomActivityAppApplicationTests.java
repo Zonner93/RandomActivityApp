6cc859caa410415e3fec91fdbd263b3eb3535520
Adam Pawluczuk <adam.pawluczuk@ppuc.pl>
2022-05-02 10:52:36 +0200

    rename test class

--- src/test/java/com/zonner/RandomActivityApp/RandomActivityAppApplicationTests.java
+++ src/test/java/com/zonner/RandomActivityApp/RandomActivityAppApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class RandomActivityAppApplicationTestsEntity {
+class RandomActivityAppApplicationTests {
 
 	@Test
 	void contextLoads() {
